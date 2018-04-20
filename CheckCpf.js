6bc96f5306852f871dd00613ef28e68bdccd2640
lfmundim <lucasfmundim@ufmg.br>
2018-04-19 21:49:08 -0300

    changed filter to a lazy verification that works on Builder's limit of operations

--- CheckCpf.js
+++ CheckCpf.js
@@ -2,9 +2,16 @@
 Deve receber o cpf extraído (e padLeft-ed com 0's até ter 11 caracteres) e sem tokens (espaço, -, ., etc)
 */
 
-function run(cpf){
-    var cpfTokens = cpf.split('').map(Number);
-    if((cpfTokens.filter()).size == 1) return false.toString();
+function run(extracted){
+    var cpfTokens = extracted.split('').map(Number);
+    var distinct = false;
+    for(i=0; i<cpfTokens.length-1; i++)
+        if(cpfTokens[i] != cpfTokens[i+1]){
+            distinct = true;
+            break;
+        }
+        
+    if(!distinct) return false.toString();
     var digits = cpfTokens.slice(0,9); //9 digits
     var verifier = cpfTokens.slice(9,11); //verifier 2 digits
     var firstSum = 0;
@@ -20,5 +27,5 @@ function run(cpf){
     if(first == 10 || first == 11) first = 0;
     if(second == 10 || second == 11) second = 0;
     if(first != verifier[0] || second != verifier[1]) return false.toString();
-    return true.toString();        
+    return true.toString();               
 }
